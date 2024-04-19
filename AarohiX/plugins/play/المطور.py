89de6c56cb6df4f5dda84615df4c44efe3b78f40
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-04-20 01:30:53 +0200

    Update المطور.py

--- AarohiX/plugins/play/المطور.py
+++ AarohiX/plugins/play/المطور.py
@@ -4,24 +4,23 @@ from strings.filters import command
 from AarohiX import app
 import config
 
-
 @app.on_message(
-    command(["مطور السورس", "مبرمج السورس", "المطور"])
+    command(["مطور السورس", "مبرمج السورس", "تيتو"])
 )
 async def maker(client: Client, message: Message):
-    await message.reply_photo(
-        photo="https://telegra.ph/file/3955f6d7c023440c11156.jpg",
-        caption="• Dev Bot ↦ سهيله \n ━━━━━━━━━━━━ \n • Dev ↦  Ahmed Teto . \n • Bio ↦ أستغفر الله الذي لا إله إلا هو الحي القيوم، وأتوب إليه . @T_S_T4",
+    await message.reply_video(
+        video="https://telegra.ph/file/9ccdf1b81efa30d2adb53.mp4",
+        caption="• ხ᥆ƚ ძᥱ᥎ᥱᥣ᥆ρᥱᖇ 🇵🇸 \n ━━━━━━━━━━━━ \n • bot ↦ {config.BOT_USER} . ",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Ahmed Teto", url=f"tg://openmessage?user_id={config.OWNER_ID}"
+                        "ძᥱ᥎ᥣ᥆ρᥱᖇ 🇵🇸", url=f"tg://openmessage?user_id={config.OWNER_ID}"
                     ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "Updates", url=config.SUPPORT_CHAT
+                        "𝗖𝗛 🇵🇸", url=config.SUPPORT_CHAT
                     ),
                 ],
             ]
