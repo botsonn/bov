fde2149ac3804f9496e21e9f42158453aa13cbcb
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-05-01 09:15:17 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -13,13 +13,13 @@ def get_string(lang: str):
 
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
-        languages["en"] = yaml.safe_load(
+        languages["ar"] = yaml.safe_load(
             open(r"./strings/langs/ar.yml", encoding="utf8")
         )
-        languages_present["ar"] = languages["en"]["name"]
+        languages_present["ar"] = languages["ar"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
+        if language_name == "ar":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
