81d78d1684fb5ce4d2e391ec18be5b1401fe1b50
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-05-05 17:56:00 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -33,6 +33,6 @@ def private_panel(_):
             )
         ],
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),            
+            )
         ],
-    ]
     return buttons
