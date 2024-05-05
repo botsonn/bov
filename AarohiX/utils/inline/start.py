9a40f2eab99bcc1e9b214e57238bf626ab752053
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-05-05 17:16:58 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -22,8 +22,7 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-        ],
-        [
+            ),
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         [InlineKeyboardButton(text=_["S_B_0"], callback_data="LG"),
         ],
