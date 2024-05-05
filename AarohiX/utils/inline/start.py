94f514e00fc3f2d01c89bf3fbcf1f71aea260fa2
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-05-05 18:27:01 +0300

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from AarohiX import app
+from BrandrdXMusic import app
 
 
 def start_panel(_):
@@ -22,19 +22,18 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-    ],
-    
-    [
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-        [InlineKeyboardButton(text=_["S_B_0"], callback_data="LG"),
-    ],
-    [
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-    ],
-    
-    [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),            
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
         ],
     ]
     return buttons
