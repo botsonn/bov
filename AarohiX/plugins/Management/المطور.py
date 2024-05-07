6781e50cb5022f3795da41a09b7b9a1b63e7c85b
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-05-08 01:21:00 +0300

    Update المطور.py

--- AarohiX/plugins/Management/المطور.py
+++ AarohiX/plugins/Management/المطور.py
@@ -10,7 +10,7 @@ import config
 async def maker(client: Client, message: Message):
     await message.reply_video(
         video="https://telegra.ph/file/9ccdf1b81efa30d2adb53.mp4",
-        caption="• ხ᥆ƚ ძᥱ᥎ᥱᥣ᥆ρᥱᖇ 🇵🇸 \n ━━━━━━━━━━━━ \n • bot ↦ {config.BOT_USER} . ",
+        caption="• ხ᥆ƚ ძᥱ᥎ᥱᥣ᥆ρᥱᖇ 🇵🇸 \n ━━━━━━━━━━━━ \n• Dev ↦  [₍ ƚ ᥱ ƚ ᥆ || تـيـ ٖ ـتـو ⁾ ↺](https://t.me/topteto) .\n• Bio ↦ أستغفر الله الذي لا إله إلا هو الحي القيوم، وأتوب إليه @T_S_T4.",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
