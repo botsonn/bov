93a8c34f0681e8fe00d67f575fa2e4a662429b01
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-05-08 00:16:16 +0300

    Update رتبتي.py

--- AarohiX/plugins/Management/رتبتي.py
+++ AarohiX/plugins/Management/رتبتي.py
@@ -1,16 +1,16 @@
 import asyncio
-
-import random
-
-from AarohiX import app
-
-from pyrogram.types import (InlineKeyboardButton,
-
-                            InlineKeyboardMarkup, Message)
-
+import os
+import time
+import requests
+import aiohttp
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-
-from pyrogram import filters, Client
+from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AarohiX import app
+from asyncio import gather
+from pyrogram.errors import FloodWait
 
 
 @app.on_message(filters.command("رتبتي", ""))
