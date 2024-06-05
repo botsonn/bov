d4f8c5ea72b3d2785f73ae069b493290c528b340
botsonn <ahmdghyly09@gmail.com>
2024-06-05 22:50:50 +0300

    Update downloader.py

--- AarohiX/utils/downloader.py
+++ AarohiX/utils/downloader.py
@@ -1,34 +1,52 @@
-from os import path
-import yt_dlp
-from yt_dlp.utils import DownloadError
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-ytdl = yt_dlp.YoutubeDL(
-    {
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-        "format": "bestaudio[ext=m4a]",
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-    }
- )
+import os
+
+from yt_dlp import YoutubeDL
+
+ydl_opts = {
+    "format": "bestaudio/best",
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "quiet": True,
+    "no_warnings": True,
+    "prefer_ffmpeg": True,
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
+        }
+    ],
+}
+ydl = YoutubeDL(ydl_opts)
 
 
-def download(url: str, my_hook) -> str:       
-    ydl_optssx = {
-        'format' : 'bestaudio[ext=m4a]',
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        'quiet': True,
-        'no_warnings': True,
-    }
-    info = ytdl.extract_info(url, False)
-    try:
-        x = yt_dlp.YoutubeDL(ydl_optssx)
-        x.add_progress_hook(my_hook)
-        dloader = x.download([url])
-    except Exception as y_e:
-        return print(y_e)
-    else:
-        dloader
-    xyz = path.join("downloads", f"{info['id']}.{info['ext']}")
-    return xyz
+def audio_dl(url: str) -> str:
+    sin = ydl.extract_info(url, False)
+    x_file = os.path.join("downloads", f"{sin['id']}.mp3")
+    if os.path.exists(x_file):
+        return x_file
+    ydl.download([url])
+    return x_file
